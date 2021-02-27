8aa3f84313c4c9567b305916440a0b06f30e2d91
GiandomenicoIameo <g.iameo@studenti.unina.it>
2021-02-27 17:00:51 +0100

    Added

---
+++ usr/include/characters.h
@@ -0,0 +1,11 @@
+#ifndef _CHARACTERS_H
+#define _CHARACTERS_H
+
+#include <stdio.h>
+
+char isvowel( char c );
+unsigned int countdigit( const char *str );
+unsigned int countcharacter( const char *str );
+unsigned int countspace( const char *str );
+
+#endif
