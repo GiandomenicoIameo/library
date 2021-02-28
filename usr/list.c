5bca5b11b9b9079590cbb1a223ab68cd65cbee4c
GiandomenicoIameo <g.iameo@studenti.unina.it>
2021-03-01 00:58:23 +0100

    Update

--- usr/list.c
+++ usr/list.c
@@ -2,11 +2,7 @@
 #include <stdlib.h>
 #include "include/list.h"
 
-struct elem* createList( void ) {
-  return NULL;
-}
-
-struct elem* createNode( int data ) {
+struct elem* create( int data ) {
 
   struct elem *node;
 
@@ -19,26 +15,61 @@ struct elem* createNode( int data ) {
   return node;
 }
 
-struct elem* push( struct elem *top, struct elem *node ) {
-
-  if( top == NULL )
-      top = node;
-  else {
-      node->next = top;
-      top = node;
-  }
-  return top;
-}
-
-struct elem* enqueue( struct elem *top, struct elem *node ) {
+struct elem* append( struct elem *top, struct elem *node ) {
 
   struct elem *res;
 
   if( top == NULL )
       res = node;
   else {
-      top->next = enqueue( top->next, node );
+      top->next = append( top->next, node );
       res = top;
+  }
+  return res;
+}
+
+struct elem *delete( struct elem *top, int data ) {
+
+  struct elem *res;
+
+  if( top == NULL )
+      res = top;
+  else {
+      if( top->data == data ) {
+          res = top->next;
+          free( top );
+      }
+      else {
+          top->next = delete( top->next, data );
+          res = top;
+      }
+  }
+  return res;
+}
+
+struct elem *search( struct elem *top, int data ) {
+
+  if( top == NULL )
+      return top;
+  else {
+      if( top->data == data )
+          return top;
+      else
+          return search( top->next, data );
+  }
+}
+
+struct elem *duplicate( struct elem *top ) {
+
+  struct elem *res;
+  struct elem *node;
+
+  if( top == NULL )
+      res = top;
+  else {
+      node = create( top->data );
+      node->next = duplicate( top->next );
+      res = node;
   }
   return res;
 }
