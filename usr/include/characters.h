21e5068e183efee921afe418fd4a7ecfe8d369ff
GiandomenicoIameo <g.iameo@studenti.unina.it>
2021-02-28 19:25:25 +0100

    Added

--- usr/include/characters.h
+++ usr/include/characters.h
@@ -1,6 +1,6 @@
 /* This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY. For more details consult the code.
-   
+
    This library is free software; you can redistribute it and/or
    modify. ISO C99 Standard: 1.0 Characters      <characters.h>
 */
@@ -13,6 +13,13 @@
 char isvowel( char c );
 unsigned int strcd( const char *s );
 unsigned int strcch( const char *s );
-unsigned int strcsp( const char *s ) ;
+unsigned int strcsp( const char *s );
+
+/* Mainly the following three basic operations
+ are performed in the stack */
+
+void strpop( char *s );
+size_t strtop( char *s );
+void strpush( char *s, int c );
 
 #endif
