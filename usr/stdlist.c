596f200c34ba246ede34c843217e5fb69479a916
GiandomenicoIameo <57752372+GiandomenicoIameo@users.noreply.github.com>
2021-03-02 06:45:30 +0100

    Update

--- usr/stdlist.c
+++ usr/stdlist.c
@@ -16,11 +16,11 @@ struct elem *create( int key ) {
 
 struct elem *push( struct elem *top, struct elem *node ) {
 
-  if( top == NULL )
-      top = node;
-  else {
-      node->next = top;
-      top = node;
+  if( top == NULL ) {
+          top = node;
+  } else {
+          node->next = top;
+          top = node;
   }
   return top;
 }
@@ -29,11 +29,11 @@ struct elem *append( struct elem *top, struct elem *node ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = node;
-  else {
-      top->next = append( top->next, node );
-      res = top;
+  if( top == NULL ) {
+          res = node;
+  } else {
+          top->next = append( top->next, node );
+          res = top;
   }
   return res;
 }
@@ -42,17 +42,16 @@ struct elem *inorder( struct elem *top, struct elem *node ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = node;
-  else {
-      if( top->data > node->data ) {
-          node->next = top;
+  if( top == NULL ) {
           res = node;
-      }
-      else {
-          top->next = inorder( top->next, node );
-          res = top;
-      }
+  } else {
+          if( top->data > node->data ) {
+                  node->next = top;
+                  res = node;
+          } else {
+                  top->next = inorder( top->next, node );
+                  res = top;
+          }
   }
   return res;
 }
@@ -61,17 +60,16 @@ struct elem *delete( struct elem *top, int key ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = top;
-  else {
-      if( top->data == key ) {
-          res = top->next;
-          free( top );
-      }
-      else {
-          top->next = delete( top->next, key );
+  if( top == NULL ) {
           res = top;
-      }
+  } else {
+          if( top->data == key ) {
+                  res = top->next;
+                  free( top );
+          } else {
+                  top->next = delete( top->next, key );
+                  res = top;
+          }
   }
   return res;
 }
@@ -80,11 +78,11 @@ struct elem *pop( struct elem *top ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = top;
-  else {
-      res = top->next;
-      free( top );
+  if( top == NULL ) {
+          res = top;
+  } else {
+          res = top->next;
+          free( top );
   }
   return res;
 }
@@ -93,13 +91,14 @@ struct elem *address( struct elem *top, int key ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = top;
-  else {
-      if( top->data == key )
+  if( top == NULL ) {
           res = top;
-      else
-          res = address( top->next, key );
+  } else {
+          if( top->data == key ) {
+                  res = top;
+          } else {
+                  res = address( top->next, key ); 
+          }
   }
   return res;
 }
@@ -109,12 +108,12 @@ struct elem *copy( struct elem *top ) {
   struct elem *res;
   struct elem *node;
 
-  if( top == NULL )
-      res = top;
-  else {
-      node = create( top->data );
-      node->next = copy( top->next );
-      res = node;
+  if( top == NULL ) {
+          res = top;
+  } else {
+          node = create( top->data );
+          node->next = copy( top->next );
+          res = node;
   }
   return res;
 }
@@ -123,15 +122,17 @@ struct elem *max( struct elem *top ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = top;
-  else {
-      res = max( top->next );
+  if( top == NULL ) {
+          res = top;
+  } else {
+          res = max( top->next );
 
-      if( res == NULL ) res = top;
-      else {
-        if( top->data > res->data ) res = top;
-      }
+          if( res == NULL ) {
+                  res = top;
+          } else {
+                 if( top->data > res->data )
+                         res = top;
+          }
   }
   return res;
 }
@@ -140,28 +141,31 @@ struct elem *min( struct elem *top ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = top;
-  else {
-      res = min( top->next );
+  if( top == NULL ) {
+          res = top;
+  } else {
+          res = min( top->next );
 
-      if( res == NULL ) res = top;
-      else {
-        if( top->data < res->data ) res = top;
-      }
+          if( res == NULL ) {
+                  res = top;
+          } else {
+                 if( top->data < res->data )
+                         res = top;
+          }
   }
   return res;
 }
+
 
 struct elem *concatenate( struct elem *top1, struct elem *top2 ) {
 
   struct elem *res;
 
-  if( top1 == NULL )
-      res = top2;
-  else {
-      top1->next = concatenate( top1->next, top2 );
-      res = top1;
+  if( top1 == NULL ) {
+          res = top2;
+  } else {
+          top1->next = concatenate( top1->next, top2 );
+          res = top1;
   }
   return res;
 }
@@ -171,20 +175,18 @@ struct elem *merge( struct elem *top1, struct elem *top2 ) {
   struct elem *res;
 
   if( top1 == NULL || top2 == NULL ) {
-      if( top1 == NULL )
-          res = top1;
-      else
-          res = top2;
-  }
-  else {
-      if( top1->data < top2->data ) {
-          top1->next = merge( top1->next, top2 );
-          res = top1;
-      }
-      else {
-          top2->next = merge( top1, top2->next );
-          res = top2;
-      }
+          if( top1 == NULL )
+                  res = top1;
+          else
+                  res = top2;
+  } else {
+          if( top1->data < top2->data ) {
+                  top1->next = merge( top1->next, top2 );
+                  res = top1;
+          } else {
+                  top2->next = merge( top1, top2->next );
+                  res = top2;
+          }
   }
   return res;
 }
@@ -193,18 +195,19 @@ struct elem *clear( struct elem *top ) {
 
   struct elem *res;
 
-  if( top == NULL )
-      res = top;
-  else {
-      res = clear( top->next );
-      free( top );
+  if( top == NULL ) {
+          res = top;
+  } else {
+          res = clear( top->next );
+          free( top );
   }
   return res;
 }
 
 int isempty( void *list ) {
 
-  if( list == NULL ) return 1;
+  if( list == NULL )
+       return 1;
   return 0;
 }
 
@@ -225,11 +228,11 @@ static struct elem *add( struct elem *end, struct elem *node ) {
 
   struct elem *res;
 
-  if( end == NULL )
-      res = node;
-  else {
-      end->next = node;
-      res = node;
+  if( end == NULL ) {
+          res = node;
+  } else {
+          end->next = node;
+          res = node;
   }
   return res;
 }
@@ -238,14 +241,14 @@ struct equeue *enqueue( struct equeue *queue, struct elem *node ) {
 
   struct equeue *res;
 
-  if( queue == NULL )
-      res = queue;
-  else {
-      queue->end = add( queue->end, node );
+  if( queue == NULL ) {
+          res = queue;
+  } else {
+          queue->end = add( queue->end, node );
 
-      if( isempty( queue->top ) )
-          queue->top = queue->end;
-      res = queue;
+          if( isempty( queue->top ) )
+                  queue->top = queue->end;
+          res = queue;
   }
   return res;
 }
@@ -254,11 +257,11 @@ struct equeue *denqueue( struct equeue *queue ) {
 
   struct equeue *res;
 
-  if( queue == NULL )
-      res = queue;
-  else {
-      queue->top = pop( queue->top );
-      res = queue;
+  if( queue == NULL ) {
+          res = queue;
+  } else {
+          queue->top = pop( queue->top );
+          res = queue;
   }
   return res;
 }
