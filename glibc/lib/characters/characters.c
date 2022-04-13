428150dec7e52b405318484916319afa1328f331
GiandomenicoIameo <g.iameo@studenti.unina.it>
2022-04-13 19:03:24 +0200

    update

--- glibc/lib/characters/characters.c
+++ glibc/lib/characters/characters.c
@@ -2,49 +2,51 @@
 
 char isvowel( char c ) {
 
-  // La funzione verifica se c è una vocale
-  switch( c ) {
+    // La funzione verifica se c è una vocale
+    switch( c ) {
 
-  case 'a' :
-  case 'A' :
+    case 'a' :
+    case 'A' :
 
-  case 'e' :
-  case 'E' :
+    case 'e' :
+    case 'E' :
 
-  case 'i' :
-  case 'I' :
+    case 'i' :
+    case 'I' :
 
-  case 'o' :
-  case 'O' :
+    case 'o' :
+    case 'O' :
 
-  case 'u' :
-  case 'U' :
-          return 1;
-  default :
-          return 0;
-  }
+    case 'u' :
+    case 'U' :
+            return 1;
+    default :
+            return 0;
+    }
 }
 
 unsigned int strcd( const char *s ) {
 
-  // strcd = (str)ing (c)ount (d)igit
-  // La funzione conta il numero di cifre in una stringa
-  unsigned int count = 0;
+    // strcd = (str)ing (c)ount (d)igit
+    // La funzione conta il numero di cifre in una stringa
+    unsigned int count = 0;
 
-  for( ; *s != '\0'; s++ ) {
-          if( isdigit( *s ) ) count++;
-  }
-  return count;
+    for( ; *s != '\0'; s++ ) {
+            if( isdigit( *s ) )
+                    count++;
+    }
+    return count;
 }
 
 unsigned int strcsp( const char *s ) {
 
-  // strcsp = (str)ing (c)ount (sp)ace
-  // La funzione conta il numero di spazi in una stringa
-  unsigned int count = 0;
+    // strcsp = (str)ing (c)ount (sp)ace
+    // La funzione conta il numero di spazi in una stringa
+    unsigned int count = 0;
 
-  for( ; *s != '\0'; s++ ) {
-          if( *s == ' ' ) count++;
-  }
-  return count;
+    for( ; *s != '\0'; s++ ) {
+            if( *s == ' ' )
+                    count++;
+    }
+    return count;
 }
